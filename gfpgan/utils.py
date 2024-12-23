05e5b2a09fc6a8b8aabafd05d1a7d02c57e0d196
Anirudh Singh <anisingh@adobe.com>
2024-12-23 22:14:32 +0530

    Update utils.py

--- gfpgan/utils.py
+++ gfpgan/utils.py
@@ -84,7 +84,7 @@ class GFPGANer():
             save_ext='png',
             use_parse=True,
             device=self.device,
-            model_rootpath='gfpgan/weights')
+            model_rootpath=os.path.dirname*(model_path))
 
         if model_path.startswith('https://'):
             model_path = load_file_from_url(
